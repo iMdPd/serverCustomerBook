d6faade52a667266dd9765f3a46fee8250061ea2
Mariusz Politowicz <politowiczmar@gmail.com>
2023-05-11 18:43:03 +0200

    Create expres.static path

--- server.js
+++ server.js
@@ -2,6 +2,10 @@ const express = require("express");
 const app = express();
 const port = 8000;
 
+const path = require("path");
+
 app.listen(port, () => {
   console.log(`Server is running on port: ${port}`);
 });
+
+app.use(express.static(path.join(__dirname, "/public")));
