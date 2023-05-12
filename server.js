552d3ca7cefc20565682a247a588b539afa8fb8b
Mariusz Politowicz <politowiczmar@gmail.com>
2023-05-12 19:59:15 +0200

    Add api endpoints for concert path

--- server.js
+++ server.js
@@ -82,6 +82,136 @@ app.delete("/testimonials/:id", (req, res) => {
   }
 });
 
+/* CONCERTS------------------------------------------------- */
+
+app.get("/concerts", (req, res) => {
+  res.json(concerts);
+});
+
+app.get("/concerts/:id", (req, res) => {
+  const { id } = req.params;
+  const getElementById = concerts.find((obj) => obj.id === +id);
+
+  if (getElementById) {
+    res.json(getElementById);
+  } else {
+    res.send("Given id doesn't exist.");
+  }
+});
+
+app.post("/concerts", (req, res) => {
+  const { performer, genre, price, day, image } = req.body;
+  const id = Math.max(...concerts.map((obj) => obj.id)) + 1;
+
+  if (performer && genre && price && day && image) {
+    concerts.push({ id, performer, genre, price, day, image });
+    res.send("Data has been successfully added!");
+    console.log(concerts);
+  } else {
+    res.send(
+      "Ooops, something went wrong. Please don't forget to fill all input fields."
+    );
+  }
+});
+
+app.put("/concerts/:id", (req, res) => {
+  const { id } = req.params;
+  const { performer, genre, price, day, image } = req.body;
+  const getElementById = concerts.find((obj) => obj.id === +id);
+
+  if (getElementById && performer && genre && price && day && image) {
+    getElementById.performer = performer;
+    getElementById.genre = genre;
+    getElementById.price = price;
+    getElementById.day = day;
+    getElementById.image = image;
+    res.send("Data has been successfully changed!");
+    console.log(concerts);
+  } else {
+    res.send(
+      "Ooops, something went wrong. Please don't forget to fill all input fields."
+    );
+  }
+});
+
+app.delete("/concerts/:id", (req, res) => {
+  const { id } = req.params;
+  const removedElement = concerts.findIndex((obj) => obj.id === +id);
+
+  if (removedElement >= 0) {
+    concerts.splice(removedElement, 1);
+    res.send("Data has been successfully removed!");
+    console.log(concerts);
+  } else {
+    res.send("Given id doesn't exist.");
+  }
+});
+
+/* CONCERTS------------------------------------------------- */
+
+app.get("/concerts", (req, res) => {
+  res.json(concerts);
+});
+
+app.get("/concerts/:id", (req, res) => {
+  const { id } = req.params;
+  const getElementById = concerts.find((obj) => obj.id === +id);
+
+  if (getElementById) {
+    res.json(getElementById);
+  } else {
+    res.send("Given id doesn't exist.");
+  }
+});
+
+app.post("/concerts", (req, res) => {
+  const { performer, genre, price, day, image } = req.body;
+  const id = Math.max(...concerts.map((obj) => obj.id)) + 1;
+
+  if (performer && genre && price && day && image) {
+    concerts.push({ id, performer, genre, price, day, image });
+    res.send("Data has been successfully added!");
+    console.log(concerts);
+  } else {
+    res.send(
+      "Ooops, something went wrong. Please don't forget to fill all input fields."
+    );
+  }
+});
+
+app.put("/concerts/:id", (req, res) => {
+  const { id } = req.params;
+  const { performer, genre, price, day, image } = req.body;
+  const getElementById = concerts.find((obj) => obj.id === +id);
+
+  if (getElementById && performer && genre && price && day && image) {
+    getElementById.performer = performer;
+    getElementById.genre = genre;
+    getElementById.price = price;
+    getElementById.day = day;
+    getElementById.image = image;
+    res.send("Data has been successfully changed!");
+    console.log(concerts);
+  } else {
+    res.send(
+      "Ooops, something went wrong. Please don't forget to fill all input fields."
+    );
+  }
+});
+
+app.delete("/concerts/:id", (req, res) => {
+  const { id } = req.params;
+  const removedElement = concerts.findIndex((obj) => obj.id === +id);
+
+  if (removedElement >= 0) {
+    concerts.splice(removedElement, 1);
+    res.send("Data has been successfully removed!");
+    console.log(concerts);
+  } else {
+    res.send("Given id doesn't exist.");
+  }
+});
+
 app.use((req, res) => {
   res.status(404).json({ message: "Not found..." });
 });
