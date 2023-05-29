ffab163bad8e054ee87b6ef47816727f2b66e7dd
Mariusz Politowicz <politowiczmar@gmail.com>
2023-05-29 18:54:53 +0200

    fix bug with undefined emit

--- server.js
+++ server.js
@@ -16,6 +16,25 @@ app.use(express.static(path.join(__dirname, "/client/build")));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+const server = app.listen(PORT, () => {
+  console.log(`Server is running on port: ${process.env.PORT || PORT}`);
+});
+
+const io = socket(server, {
+  cors: {
+    origin: "*",
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log("New client! Its id – " + socket.id);
+});
+
+app.use((req, res, next) => {
+  req.io = io;
+  next();
+});
+
 const dbURL = `mongodb+srv://${process.env.USERNAME}:${process.env.PASSWORD}@${process.env.DATABASE}.r4eleyk.mongodb.net/${process.env.COLLECTION}?retryWrites=true&w=majority`;
 
 const connectionParams = {
@@ -41,23 +60,4 @@ app.get("*", (req, res) => {
 
 app.use((req, res) => {
   res.status(404).json({ message: "Not found..." });
-});
-
-const server = app.listen(PORT, () => {
-  console.log(`Server is running on port: ${process.env.PORT || PORT}`);
-});
-
-const io = socket(server, {
-  cors: {
-    origin: "*",
-  },
-});
-
-io.on("connection", (socket) => {
-  console.log("New client! Its id – " + socket.id);
-});
-
-app.use((req, res, next) => {
-  req.io = io;
-  next();
 });
