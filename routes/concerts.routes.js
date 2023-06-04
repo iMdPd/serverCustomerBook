91b4e7a1512b20a4f46d27d67ea68ea826d84a49
Mariusz Politowicz <politowiczmar@gmail.com>
2023-06-04 15:48:28 +0200

    Update concerts delete route

--- routes/concerts.routes.js
+++ routes/concerts.routes.js
@@ -10,21 +10,6 @@ router.route("/concerts").post(ConcertController.post);
 
 router.route("/concerts/:id").put(ConcertController.put);
 
-router.route("/concerts/:id").delete((req, res) => {
-  const { id } = req.params;
-  const removedElement = concerts.findIndex((obj) => obj.id === +id);
-
-  if (removedElement >= 0) {
-    concerts.splice(removedElement, 1);
-
-    res.status(200).json({
-      message: "Data has been successfully removed!",
-    });
-
-    console.log(concerts);
-  } else {
-    res.status(404).json({ message: "Given id doesn't exist." });
-  }
-});
+router.route("/concerts/:id").delete(ConcertController.delete);
 
 module.exports = router;
